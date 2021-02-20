60f53578a1e40f26ee8bac4af3c653992ddfdd3e
benlcollins <ben@benlcollins.com>
2021-02-20 09:58:34 -0500

    add lesson 30 code for multiple import from drive folder

---
+++ 030_importMultipleCSVFromDrive.js
@@ -0,0 +1,60 @@
+/**
+ * custom menu
+ */
+function onOpen() {
+  
+  const ui = SpreadsheetApp.getUi();
+  ui.createMenu('Import CSV')
+    .addItem('Import from file...', 'importCSVFromFile')
+    .addItem('Import from folder...','importCSVFromFolder')
+    .addToUi();
+  
+}
+
+
+/**
+ * extract csv data from any files in a named Drive folder
+ */
+function importCSVFromFolder() {
+
+  // find the folder with the CSVs
+  const folder = DriveApp.getFolderById('16MhsGFia-WMhzHkOO5W8QgjVSObu9bn4');
+
+  // extract the CSV files
+  const csvFiles = folder.getFilesByType(MimeType.CSV); // FileIterator
+  Logger.log(csvFiles.hasNext()); 
+
+  // loop over file iterator whilst it still has another file in
+  while (csvFiles.hasNext()) {
+
+    // extract the data from file
+    const file = csvFiles.next();
+    importCSVFromFile(file);
+
+  }
+}
+
+/**
+ * Import CSV data from individual file
+ */
+function importCSVFromFile(file) {
+
+  // get hold of the CSV file
+  //const fileName = 'testing_csv_1.csv';
+  //const file = DriveApp.getFilesByName(fileName).next();
+
+  // get the data
+  const dataBlob = file.getBlob();
+  const csvString = dataBlob.getDataAsString();
+  const csvData = Utilities.parseCsv(csvString);
+
+  // remove the header row
+  csvData.shift();
+  console.log(csvData);
+
+  // paste it into our Google Sheet
+  const sheet = SpreadsheetApp.getActiveSheet();
+  const lastRow = sheet.getLastRow();
+  sheet.getRange(lastRow + 1,1,csvData.length,csvData[0].length).setValues(csvData);
+
+}
